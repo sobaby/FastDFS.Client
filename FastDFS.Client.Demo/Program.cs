c8cac45e685582325e1be27a35ab49efa4a8c6ee
SmartBooks <SmartBooks@qq.com>
2014-07-14 15:49:25 +0800

    import

---
+++ FastDFS.Client.Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastDFS.Client.Common;
+using FastDFS.Client.Config;
+
+namespace FastDFS.Client.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = FastDfsManager.GetConfigSection();
+
+            StorageNode storageNode = null;
+
+            var fileName = "";
+
+            do
+            {
+                Console.WriteLine("1.Init");
+                Console.WriteLine("2.GetStorageNode");
+                Console.WriteLine("3.UploadFile");
+                Console.WriteLine("4.RemoveFile");
+
+                Console.Write("请输入命令：");
+                var cmd = Console.ReadLine();
+
+                switch (cmd)
+                {
+                    case "1":
+                        ConnectionManager.InitializeForConfigSection(config);
+                        break;
+
+                    case "2":
+                        storageNode = FastDFSClient.GetStorageNode(config.GroupName);
+                        Console.WriteLine(storageNode.EndPoint);
+                        break;
+
+                    case "3":
+                        fileName = FastDFSClient.UploadFile(storageNode, File.ReadAllBytes("test.jpg"), "jpg");
+                        Console.WriteLine(fileName);
+                        break;
+
+                    case "4":
+                        FastDFSClient.RemoveFile(config.GroupName, fileName);
+                        break;
+                }
+
+            } while (true);
+
+        }
+    }
+}
---
+++ FastDFS.Client/Common/Connection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace FastDFS.Client.Common
+{
+    public class Connection : TcpClient
+    {
+        public Connection()
+        {
+            InUse = false;
+        }
+
+        public Pool Pool { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime LastUseTime { get; set; }
+
+        public bool InUse { get; set; }
+
+        public void Open()
+        {
+            if (InUse)
+            {
+                throw new FDFSException("the connection is already in user");
+            }
+
+            InUse = true;
+            LastUseTime = DateTime.Now;
+        }
+
+        public new void Close()
+        {
+            Pool.CloseConnection(this);
+        }
+
+        public void Release()
+        {
+            Pool.ReleaseConnection(this);
+        }
+    }
+}
